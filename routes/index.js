ee0ef5fcfeda3055e945338f7e7084db406c363d
Carol Kang <otherhats@gmail.com>
2022-10-04 17:01:00 -0400

    add an about view

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page' })
 })
 
+router.get('/about', function (req, res) {
+  res.render('about', { title: 'About' })
+})
+
 export {
   router
 }
