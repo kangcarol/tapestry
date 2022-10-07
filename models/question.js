edb120d3d0357e87c0124674ce09c5962ecc33a7
Carol Kang <otherhats@gmail.com>
2022-10-07 01:35:23 -0400

    require question not be blank, in order to add

--- models/question.js
+++ models/question.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const questionSchema = new Schema({
-  text: String,
+  text: { type: String, required: true },
   answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }], //answers is for icebox: view page per question with all of its answers
 })
 
